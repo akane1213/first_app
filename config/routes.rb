7a552607e1daec374b8dd6adb295b3dd11441667
akane1213 <76610860+akane1213@users.noreply.github.com>
2020-12-26 19:15:09 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,82 @@
+Rails.application.routes.draw do
+  get 'posts' , to: 'posts#index'
+  get 'posts/new' , to: 'posts#new'
+  post 'posts', to: 'posts#create'
+end
+
+
+配列 = []
+配列[num]
+
+
+ハッシュ = {}
+ハッシュ[:aaa] = bbb
+
+ハッシュ[:aaa]
+
+i = 0
+m = 4
+n = 1
+
+4.times do
+  i = 0 + 1
+  i = 1 + 1
+  i = 2 + 1
+  i = 3 + 1
+    = 4
+end
+
+j = 4
+
+
+j = 4
+x = 10
+i = 2
+
+--- 
+
+array = [0, 3, 5, 1]
+
+array = [0, 3, 5, 1, 1]
+array = [0, 3, 5, 1, 1, 1]
+array = [0, 3, 5, 1, 1, 1, 1]
+
+[1, 1, 1].each do |element|
+  if element == 1
+    count += 1
+  end
+end
+
+if 0 == 1
+  
+end
+
+if 3 == 1
+  
+end
+
+if 5 == 1
+  
+end
+
+if 1 == 1
+  count = 1
+end
+
+if 1 == 1
+  count = 1 + 1
+        = 2
+end
+
+if 1 == 1
+  count = 2 + 1
+        = 3
+end
+
+if 1 == 1
+  count = 3 + 1
+        = 4
+end
+
+count = 4
+
